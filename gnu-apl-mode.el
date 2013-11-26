5601b1c422990b372d9c36ebe0d51632c0889ec4
Elias Martenson <lokedhs@gmail.com>
2013-11-26 11:22:12 +0800

    Further work on implementing the full keymap

--- gnu-apl-mode.el
+++ gnu-apl-mode.el
@@ -2,16 +2,65 @@
 
 (require 'cl)
 
-(makunbound 'gnu-apl--symbols)
-(defvar gnu-apl--symbols '(("iota" "⍳" "s-i")
-                           ("omega" "⍵" "s-w")
-                           ("omega-underbar" "⍹")
-                           ("leftarrow" "←" "s-[")
-                           ("rightarrow" "→" "s-]")
-                           ("rho" "⍴")))
+;;;###autoload
+(defcustom gnu-apl-executable "apl"
+  "Where the GNU APL implementaion is located."
+  :type 'string
+  :group 'gnu-apl)
+
+(defvar gnu-apl--symbols '(;; Top row
+                           ("diamond" "⋄" "`")
+                           ("commabar" "⍪" "S-`")
+                           ("diaeresis" "¨" "1")
+                           ("identical-to" "≡" "S-1")
+                           ("macron" "¯" "2")
+                           ("not-identical-to" "≢" "S-2")
+                           ("less-than" "<" "3")
+                           ("del-stile" "⍒" "S-3")
+                           ("less-than-or-equal-to" "≤" "4")
+                           ("delta-stile" "⍋" "S-4")
+                           ("equals" "=" "5")
+                           ("circle-stile" "⌽" "S-5")
+                           ("greater-than-or-equal-to" "≥" "6")
+                           ("circle-backslash" "⍉" "S-6")
+                           ("greater-than" ">" "7")
+                           ("circled-minus" "⊖" "S-7")
+                           ("not-equal-to" "≠" "8")
+                           ("circle-star" "⍟" "S-8")
+                           ("logical-or" "∨" "9")
+                           ("down-caret-tilde" "⍱" "S-9")
+                           ("logical-and" "∧" "0")
+                           ("up-caret-tilde" "⍲" "S-0")
+                           ("multiplication-sign" "×" "-")
+                           ("quad-colon" "⍠" "S--")
+                           ("division-sign" "÷" "=")
+                           ("quad-divide" "⌹" "S-=")
+                           ;; First row
+                           ("iota" "⍳" "i")
+                           ("iota-underbar" "⍸" "S-i")
+                           ("epsilon" "ε" "e")
+                           ("epsilon-underbar" "⍷" "S-e")
+                           ("omega" "⍵" "w")
+                           ("omega-underbar" "⍹" "S-w")
+                           ("tilde" "~" "t")
+                           ("tilde-diaeresis" "⍨" "S-t")
+                           ("uparrow" "↑" "y")
+                           ("downarrow" "↓" "u")
+                           ("circle" "○" "o")
+                           ("circle-diaeresis" "⍥" "S-o")
+                           ("pi" "π" "p")
+                           ("star-diaeresis" "⍣" "S-p")
+                           ("leftarrow" "←" "[")
+                           ("quote-quad" "⍞" "S-[")
+                           ("rightarrow" "→" "]")
+                           ("zilde" "⍬" "S-]")
+                           ("right-tack" "⊢" "\\")
+                           ("left-tack" "⊣" "S-\\")
+                           ("rho" "⍴" "r")
+                           ("root" "√" "S-r")))
 
 (defun gnu-apl--make-key-command-sym (n)
-  (intern (concat "gnu-apl-insert-sym-" n)))
+  (intern (concat "insert-sym-apl-" n)))
 
 (macrolet ((make-insert-functions ()
              `(progn
@@ -22,18 +71,16 @@
                           gnu-apl--symbols))))
   (make-insert-functions))
 
-(defun gnu-apl-insert-sym-iota ()
-  (interactive)
-  (insert-string "⍳"))
-
-(makunbound 'gnu-apl-mode-map)
-(defvar gnu-apl-mode-map
+(defun gnu-apl--make-mode-map (prefix)
   (let ((map (make-sparse-keymap)))
     (dolist (command gnu-apl--symbols)
       (let ((key-sequence (caddr command)))
         (when key-sequence
-          (define-key map (kbd key-sequence) (gnu-apl--make-key-command-sym (car command))))))
+          (define-key map (kbd (concat prefix key-sequence)) (gnu-apl--make-key-command-sym (car command))))))
     map))
+
+(defvar gnu-apl-mode-map
+  (gnu-apl--make-mode-map "s-"))
 
 (define-derived-mode gnu-apl-mode comint-mode "GNU APL"
   "Major mode for interacting with GNU APL."
@@ -42,10 +89,10 @@
 
 (defun gnu-apl ()
   (interactive)
-  (let ((buffer (get-buffer-create "*apl*")))
+  (let ((buffer (get-buffer-create "*gnu-apl*")))
     (pop-to-buffer-same-window buffer)
     (unless (comint-check-proc buffer)
-      (make-comint-in-buffer "apl" buffer "/Users/elias/src/apl/dist/bin/apl"
+      (make-comint-in-buffer "apl" buffer gnu-apl-executable
                              nil
                              "--noCIN")
       (gnu-apl-mode))))
