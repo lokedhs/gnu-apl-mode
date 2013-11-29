7a20fc2ff65df266a112835cb9e74b02da3622bd
Elias Martenson <lokedhs@gmail.com>
2013-11-29 21:14:56 +0800

    Added font workaround for OSX

---
+++ gnu-apl-osx-workaround.el
@@ -0,0 +1,8 @@
+;;; -*- lexical-binding: t -*-
+
+(defun gnu-apl-update-fontset-character (spec)
+  (dolist (s gnu-apl--symbols)
+    (let ((char (aref (second s) 0)))
+      (when (> char 255)
+        (set-fontset-font t (cons char char) (font-spec :family spec)))))
+  (set-fontset-font t '(#x2500 . #x2594) (font-spec :family spec)))
