6dfd2c680d10cb648a5fb814d7aece9e86bfb745
Elias Martenson <lokedhs@gmail.com>
2013-12-04 13:16:17 +0800

    Updated the prompt regex to prevent a hang when defining functions

--- interactive-gnu-apl.el
+++ interactive-gnu-apl.el
@@ -28,7 +28,8 @@ or NIL if there is no active session.")
   :syntax-table gnu-apl-mode-syntax-table
   :group 'gnu-apl
   (use-local-map gnu-apl-interactive-mode-map)
-  (setq comint-prompt-regexp "^\\(      \\)\\|\\(\\[[0-9]+\\] \\)")
+  ;;(setq comint-prompt-regexp "^\\(      \\)\\|\\(\\[[0-9]+\\] \\)")
+  (setq comint-prompt-regexp "^\\(      \\)?")
   (setq comint-process-echoes nil))
 
 (defun gnu-apl ()
