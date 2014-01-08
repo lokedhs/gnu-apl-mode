fbaed66cd52874f68fd0bebca4795149ac93f806
Elias Martenson <lokedhs@gmail.com>
2014-01-08 19:17:25 +0800

    Implemented "variables" command that returns all symbols.

--- native/VariablesCommand.cc
+++ native/VariablesCommand.cc
@@ -6,7 +6,18 @@ void VariablesCommand::run_command( NetworkConnection &conn, const std::vector<s
 {
     SymbolTable symbol_table = Workspace::get_symbol_table();
     stringstream out;
-    symbol_table.list_symbols( out, LIST_VARS, UCS_string( "" ) );
+
+    int num_symbols = symbol_table.symbols_allocated();
+    Symbol *symbols[num_symbols];
+    symbol_table.get_all_symbols( symbols, num_symbols );
+    for( int i = 0 ; i < num_symbols ; i++ ) {
+        Symbol *symbol = symbols[i];
+//        if( !symbol->is_erased() && symbol->value_stack.back().name_class == NC_VARIABLE ) {
+        if( !symbol->is_erased() ) {
+            out << symbol->get_name() << "\n";
+        }
+    }
+
     conn.write_string_to_fd( out.str() );
-    conn.write_string_to_fd( "\n" END_TAG "\n" );
+    conn.write_string_to_fd( END_TAG "\n" );
 }
