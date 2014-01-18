1f07ad1f208e04fe05f87a9c9b03ac492b00f8c7
Elias Martenson <lokedhs@gmail.com>
2014-01-19 01:01:10 +0800

    Further work of gnuplot integration

--- gnu-apl-plot.el
+++ gnu-apl-plot.el
@@ -32,18 +32,40 @@ from the APL runtime"
        (or (numberp (car value))
            (listp (car value)))))
 
+(defun gnu-apl--plot-insert-cell (entry)
+  (etypecase entry
+    (integer (insert (format "%d" entry)))
+    (number (insert (format "%f" entry)))
+    (string (insert (format "\"%s\"" entry)))
+    (list (cond ((listp entry)
+                 (error "Cell contains array"))
+                ((eq (car entry) :unicode)
+                 (insert (char-to-string (cadr entry))))
+                (t
+                 (error "Unknown cell content: %S" entry))))))
+
 (gnu-apl--define-variable-reading-function (gnu-apl-plot-line result)
   (unless (gnu-apl--single-dimension-p result)
     (user-error "Line plots are only supported for one-dimensional values"))
   (gnu-apl--with-temp-files ((script-file "script")
                              (data-file "data"))
     (with-temp-buffer
-      (dolist (entry result)
-        (etypecase entry
-          (integer (insert (format "%d" entry)))
-          (number (insert (format "%f" entry)))
-          (string (insert (format "\"%s\"" entry))))
-        (insert "\n"))
+      (cond ((gnu-apl--single-dimension-p result)
+             (loop for entry in result
+                   do (gnu-apl--plot-insert-cell entry)
+               (insert "\n")))
+            ((and (eq (car result) :array)
+                  (= (length (cadr result)) 2))
+             (loop for row in (caddr result)
+                   do (loop for entry in row
+                            for first = t then nil
+                            unless first
+                            do (insert " ")
+                            do (gnu-apl--plot-insert-cell entry))
+                   do (insert "\n")))
+            (t
+             (error "Unsupported content")))
+
       (write-file data-file))
     (with-temp-buffer
       (insert "plot \"" data-file "\" title \"Data from variable\" with lines\n")
