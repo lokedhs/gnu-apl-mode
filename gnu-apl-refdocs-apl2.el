264d11f53a919571b050244bb9effdedfee9de2b
Elias Martenson <lokedhs@gmail.com>
2014-02-04 11:40:42 +0800

    Fixed broken refdocs

--- gnu-apl-refdocs-apl2.el
+++ gnu-apl-refdocs-apl2.el
@@ -1122,21 +1122,21 @@ Returns L (discarding R)")
 
     ;; ========================================
 
-    (("¨"
-      ("Monadic axis" "Each" "Applies the function LO to each item of R"
-       "Z←LO¨ R
+    ("¨"
+     ("Monadic axis" "Each" "Applies the function LO to each item of R"
+      "Z←LO¨ R
 Applies the function LO to each item of R
 
  ⍴Z ←→ ⍴R
 ⍴⍴Z ←→ ⍴⍴R")
-      ("Dyadic axis" "Each" "Applies the function LO between corresponding pairs of items of L and R"
-       "Z←L LO¨ R
+     ("Dyadic axis" "Each" "Applies the function LO between corresponding pairs of items of L and R"
+      "Z←L LO¨ R
 Applies the function LO between corresponding pairs of items of L
 and R
 
 LO: Dyadic function
 
  ⍴Z ←→ ⍴R or ⍴L
-⍴⍴Z ←→ ⍴⍴R or ⍴⍴L"))
+⍴⍴Z ←→ ⍴⍴R or ⍴⍴L")
      t))
   "Documentation for APL symbols.")
