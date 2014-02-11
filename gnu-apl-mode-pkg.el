8120c96cf3abaf2551d2ce8e3006096176ef9b7b
Elias Martenson <lokedhs@gmail.com>
2014-02-11 12:44:52 +0800

    Updated version and added some comments

--- gnu-apl-mode-pkg.el
+++ gnu-apl-mode-pkg.el
@@ -1,2 +1,8 @@
-(define-package "gnu-apl-mode" "1.0" "Integrate GNU APL with Emacs"
+;;; gnu-apl-mode --- GNU APL integration for Emacs
+
+;; Copyright 2013-2014 Elias Martenson
+
+;; Author: Elias Martenson <lokedhs@gmail.com>
+;; URL: https://github.com/lokedhs/gnu-apl-mode
+(define-package "gnu-apl-mode" "1.2.0" "Integrate GNU APL with Emacs"
   nil)
