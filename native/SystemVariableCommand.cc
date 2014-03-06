9e48113271995f4fd87b3881b7c51fb895e792c3
Elias Mårtenson <lokedhs@gmail.com>
2014-03-06 22:55:09 +0800

    Include system functions in the cal to systemvariables

--- native/SystemVariableCommand.cc
+++ native/SystemVariableCommand.cc
@@ -28,6 +28,7 @@ void SystemVariableCommand::run_command( NetworkConnection &conn, const std::vec
 
 #define ro_sv_def(VAR) out << id_name( ID_ ## VAR ) << "\n";
 #define rw_sv_def(VAR) out << id_name( ID_ ## VAR ) << "\n";
+#define sf_def(VAR) out << id_name( ID_ ## VAR ) << "\n";
 #include "SystemVariable.def"
 
     out << END_TAG << "\n";
