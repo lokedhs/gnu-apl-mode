37482fe2c96a8cd0e227c1c9dcb58a9e9a06f14b
Elias Martenson <lokedhs@gmail.com>
2014-04-05 20:39:21 +0800

    Add include paths for OSX

--- native/.dir-locals.el
+++ native/.dir-locals.el
@@ -1,4 +1,6 @@
 ((c++-mode . ((eval . (progn
                         (em-append-include-dirs (list (expand-file-name "~/src/apl/src")))
+                        (when (eq system-type 'darwin)
+                          (em-append-include-dirs (list "/usr/local/Cellar/gettext/0.18.3.2/include")))
                         (flycheck-mode 1)
                         (company-mode 1))))))
