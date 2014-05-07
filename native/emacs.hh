4f1a2791c8d0871ded32ebfa0fe23bc65e307f3d
Elias Martenson <lokedhs@gmail.com>
2014-05-07 13:27:16 +0800

    Updated protocol version to 1.4

--- native/emacs.hh
+++ native/emacs.hh
@@ -36,7 +36,7 @@
 
 void set_active( bool v );
 
-#define PROTOCOL_VERSION "1.3"
+#define PROTOCOL_VERSION "1.4"
 
 #define END_TAG "APL_NATIVE_END_TAG"
 #define NOTIFICATION_START_TAG "APL_NATIVE_NOTIFICATION_START"
