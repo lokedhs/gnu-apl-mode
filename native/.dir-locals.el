885396ad8387435083e659498b88c5915a81eebb
Elias Mårtenson <lokedhs@gmail.com>
2014-05-21 12:16:06 +0800

    Fix the include path for flycheck

--- native/.dir-locals.el
+++ native/.dir-locals.el
@@ -1,5 +1,5 @@
 ((c++-mode . ((eval . (progn
-                        (em-append-include-dirs (list (expand-file-name "~/src/apl/src")))
+                        (em-append-include-dirs (list (expand-file-name "~/src/apl/src/native")))
                         (when (eq system-type 'darwin)
                           (em-append-include-dirs (list "/usr/local/Cellar/gettext/0.18.3.2/include")))
                         (flycheck-mode 1)
