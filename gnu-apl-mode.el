4d85b8758b535f43e1a338253b0896228f771555
Elias Martenson <lokedhs@gmail.com>
2014-08-11 11:35:05 +0800

    Renamed modeline text to GNU-APL

--- gnu-apl-mode.el
+++ gnu-apl-mode.el
@@ -281,7 +281,7 @@ function or nil if the function could not be parsed."
           when (string-match (concat "^" pattern) line)
           return (match-string 1 line))))
 
-(define-derived-mode gnu-apl-mode prog-mode "APL"
+(define-derived-mode gnu-apl-mode prog-mode "GNU-APL"
   "Major mode for editing GNU APL files."
   :syntax-table gnu-apl-mode-syntax-table
   :group 'gnu-apl
