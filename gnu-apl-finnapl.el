5ad8b0acf14ad29a1b302cc2b28564e764a8f40a
Alexey Veretennikov <txm.fourier@gmail.com>
2018-01-13 20:25:15 +0100

    Added support for helm and fixed bug with double first entry.

--- gnu-apl-finnapl.el
+++ gnu-apl-finnapl.el
@@ -3,9 +3,19 @@
 (require 'cl)
 (require 'tabulated-list)
 
+(eval-when-compile (require 'cl-lib))
+;; optional helm dependency
+(require 'helm nil t)
+
 (defvar *gnu-apl--finapl-url*
   "https://aplwiki.com/FinnAplIdiomLibrary?action=raw"
   "Url of the page containing FinnAPL idioms list")
+
+(defvar *gnu-apl-finapl-use-helm-choice* t
+  "Determines how to present the list of idioms.
+If t, use helm if available.
+If nil, always use internal tabular mode.")
+
 
 (defvar *gnu-apl--finnapl-idioms*
   nil
@@ -18,10 +28,12 @@ variations)
 - parameters
 - idiom itself - APL expression")
 
-(defvar-local *gnu-apl--finnapl-prev-buffer*
+(defvar *gnu-apl--finnapl-prev-buffer*
   nil
   "The buffer to insert the idiom. It is set to any buffer
 which was active then `gnu-apl-finnapl-list' was called")
+
+(defvar *gnu-apl--finnapl-title* "*APL Idioms*")
 
 (defun gnu-apl-finnapl-download-list ()
   "Download the FinnAPL idioms list and parse them
@@ -31,39 +43,41 @@ containing parsed values from this list"
                 #'gnu-apl--parse-finnapl-text-webpage))
 
 
- (defun gnu-apl--parse-finnapl-text-webpage (status)
-   (unless (plist-member status :error)
-     ;; clear the old idioms list
-     (setq *gnu-apl--finnapl-idioms* nil)
-     ;; start with the beginning of the buffer and
-     ;; find the beginnin of the list
-     (goto-char (point-min))
-     (let* ((start-text "== Idiom Library Listing ==")
-            (start-point (search-forward start-text (point-max) t)))
-       ;; collect all sections
-       (when start-point
-         (let* ((section-regexp "^=== \\(.*\\) ===$")
-                (sections
-                 (loop for index-start = 0 then (match-end 0)
-                       for last-section-name = nil then (decode-coding-string (match-string 1) 'utf-8)
-                       while (search-forward-regexp section-regexp
-                                                    (point-max)
-                                                    t)
-                       when last-section-name
-                       collect (list last-section-name
-                                     index-start
-                                     (match-beginning 0))
-                       into result
-                       finally
-                       (return (nconc result (list (list last-section-name index-start (point-max))))))))
-           ;; parse sections updating the global list of idioms
-           (dolist (x sections)
-             (apply #'gnu-apl--parse-finnapl-section x))
-           ;; kill the buffer created by url-retrieve. 
-           (kill-buffer (current-buffer))
-           (setq *gnu-apl--finnapl-idioms* (nreverse *gnu-apl--finnapl-idioms*))
-           (message "List of APL idioms successfully downloaded")
-           (gnu-apl-finnapl-choice-tabular))))))
+(defun gnu-apl--parse-finnapl-text-webpage (&optional status)
+  (unless (plist-member status :error)
+    ;; clear the old idioms list
+    (setq *gnu-apl--finnapl-idioms* nil)
+    ;; start with the beginning of the buffer and
+    ;; find the beginnin of the list
+    (goto-char (point-min))
+    (let* ((start-text "== Idiom Library Listing ==[ \n\r]*")
+           (start-point (search-forward-regexp start-text (point-max) t)))
+      ;; collect all sections
+      (when start-point
+        (let* ((section-regexp "^=== \\(.*\\) ===[ \n\r]*")
+               (sections
+                (loop for index-start = 0 then (match-end 0)
+                      for last-section-name = nil then (decode-coding-string (match-string 1) 'utf-8)
+                      while (search-forward-regexp section-regexp
+                                                   (point-max)
+                                                   t)
+                      when last-section-name
+                      collect (list last-section-name
+                                    index-start
+                                    (match-beginning 0))
+                      into result
+                      finally
+                      (return (nconc result (list (list last-section-name index-start (point-max))))))))
+          ;; parse sections updating the global list of idioms
+          (dolist (x sections)
+            (apply #'gnu-apl--parse-finnapl-section x))
+          ;; kill the buffer created by url-retrieve. 
+          (kill-buffer (current-buffer))
+          (setq *gnu-apl--finnapl-idioms* (nreverse *gnu-apl--finnapl-idioms*))
+          (message "List of APL idioms successfully downloaded")
+          (if (and *gnu-apl-finapl-use-helm-choice* (fboundp 'helm))
+              (gnu-apl-finnapl-choice-helm)
+            (gnu-apl-finnapl-choice-tabular)))))))
 
 
 
@@ -112,7 +126,6 @@ The IDIOM is a list of:
   - end position of idiom contents"
   (destructuring-bind (id name args numrows start end)
       idiom
-    (print name)
     (let* ((idiom-row-regexp "colspan=[0-9].*>\\(.*\\)[ \t]*||")
            (rows
             (save-excursion
@@ -155,6 +168,12 @@ The IDIOM is a list of:
                                   ("Idiom" 0 nil)])
     (tabulated-list-init-header)))
 
+(defun gnu-apl-finnapl--insert-idiom (idiom)
+  (with-current-buffer *gnu-apl--finnapl-prev-buffer*
+    (insert idiom)
+    (goto-char (+ (point) (length idiom)))))
+
+
 
 (defun gnu-apl-finnapl-choice-tabular-action (button)
   "Action on either Enter key or mouse1.
@@ -163,16 +182,13 @@ and closes the idioms window."
   (ignore button)
   (let* ((id (tabulated-list-get-id))
          (idiom (fourth (cl-find id *gnu-apl--finnapl-idioms*
-                              :test #'string= :key #'car))))
+                                 :test #'string= :key #'car))))
     (quit-window t)  
-    (with-current-buffer *gnu-apl--finnapl-prev-buffer*
-      (insert idiom)
-      (goto-char (+ (point) (length idiom))))))
-
+    (gnu-apl-finnapl--insert-idiom idiom)))
 
 (defun gnu-apl-finnapl-choice-tabular ()
   "Create a buffer with a list of alternatives to choose from."
-  (let ((buf (get-buffer-create "*APL Idioms*")))
+  (let ((buf (get-buffer-create *gnu-apl--finnapl-title*)))
     ;; first create and switch to the new empty buffer
     (switch-to-buffer buf)
     ;; turn on the choices mode (based on tabulated-list)
@@ -184,16 +200,16 @@ and closes the idioms window."
     ;; The action on any of these lines is the same - to insert
     ;; an idiom into the GNU APL interpreter buffer
     (cl-flet ((create-line (id col1 col2 &optional (face 'default))
-                        (list id
-                              (vector
-                               (cons col1
-                                     (list 'action
-                                           #'gnu-apl-finnapl-choice-tabular-action
-                                           'face face))
-                               (cons col2
-                                     (list 'action
-                                           #'gnu-apl-finnapl-choice-tabular-action
-                                           'face face))))))
+                           (list id
+                                 (vector
+                                  (cons col1
+                                        (list 'action
+                                              #'gnu-apl-finnapl-choice-tabular-action
+                                              'face face))
+                                  (cons col2
+                                        (list 'action
+                                              #'gnu-apl-finnapl-choice-tabular-action
+                                              'face face))))))
       (setq tabulated-list-entries
             (loop for (id name args idiom) in *gnu-apl--finnapl-idioms*
                   ;; first line is the idiom id and a name
@@ -209,13 +225,38 @@ and closes the idioms window."
 (defun gnu-apl-finnapl-list ()
   "Download and present the list of FinnAPL idioms.
 User can click or press Enter on any row to insert idiom into
-the GNU APL"
+the GNU APL.
+
+The idioms list is downloaded from the APL Wiki:
+https://aplwiki.com/FinnAplIdiomLibrary"
   (interactive)
+  (setq *gnu-apl--finnapl-prev-buffer* (current-buffer))
   (save-selected-window
-    (setq *gnu-apl--finnapl-prev-buffer* (current-buffer))
-    (if (not *gnu-apl--finnapl-idioms*)
-        (gnu-apl-finnapl-download-list)
-      (gnu-apl-finnapl-choice-tabular))))
+    (cond ((not *gnu-apl--finnapl-idioms*)
+           (gnu-apl-finnapl-download-list))
+          ((and *gnu-apl-finapl-use-helm-choice* (fboundp 'helm))
+           (gnu-apl-finnapl-choice-helm))
+          (t (gnu-apl-finnapl-choice-tabular)))))
+
+
+
+(defun gnu-apl-finnapl-choice-helm ()
+  "Present helm narrowing search buffer for FinnAPL idioms"
+  (let* ((candidates
+          (mapcar (lambda(x)
+                    (cons (concat (car x) ". " (second x) "\n"
+                                  (third x) "\n" (fourth x))
+                          (fourth x)))
+                  *gnu-apl--finnapl-idioms*))
+         (sources
+          `((name . "FinnAPL Idioms")
+            (multiline . t)
+            (candidates . ,candidates)
+            (action . (lambda (x) (gnu-apl-finnapl--insert-idiom x))))))
+    (let ((helm-candidate-number-limit nil))
+      (helm :sources sources
+            :prompt "Search for: "
+            :buffer *gnu-apl--finnapl-title*))))
 
 
 (provide 'gnu-apl-finnapl)
